44cb066f50d3d2b8abedc501c2e0e22f32bb34cc
Richard F Campos <richardfcampos@gmail.com>
2025-01-07 10:57:48 -0300

    fix: modal emitions when closing modals

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -26,26 +26,27 @@ watch(
 const closeModal = () => {
     isModalVisible.value = false;
     // Emit the updated value back to parent
-    emit('update:modelValue', false);
+    emit('update:modelValue', false); // Properly signal the parent to close the modal
 };
 
 // Method to open the modal
 const openModal = () => {
     isModalVisible.value = true;
     // Emit the updated value back to parent
-    emit('update:modelValue', true);
+    emit('update:modelValue', true); // Properly signal the parent to open the modal
 };
 </script>
 
 <template>
     <VueFinalModal
         v-model="isModalVisible"
+        @before-close="closeModal"
         class="flex items-center justify-center"
         content-class="flex flex-col p-4 bg-white rounded border border-gray-100 dark:border-gray-800"
     >
-        <div class="bg-white">
+        <div class="bg-white relative">
+            <button @click="closeModal" class="absolute right-4 top-4">X</button>
             <slot></slot>
-            <button @click="closeModal">Close</button>
         </div>
     </VueFinalModal>
 </template>
